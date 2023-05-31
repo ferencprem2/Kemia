45d7b4aa687f503207b23109715de52c81b15286
Mechwart András <mechwwart@gmail.com>
2023-05-31 10:06:25 +0200

    Befejezés

---
+++ Elemek.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga
+{
+    class Elemek
+    {
+        private string ev;
+        private string elem;
+        private string vegyjel;
+        private int rendszam;
+        private string felfedezo;
+
+        public Elemek(string csvLine)
+        {
+            var lines = csvLine.Split(';');
+
+            ev = lines[0];
+            elem = lines[1];
+            vegyjel = lines[2];
+            rendszam = Convert.ToInt32(lines[3]);
+            felfedezo = lines[4];
+        }
+
+        public Elemek(string ev, string elem, string vegyjel, int rendszam, string felfedezo)
+        {
+            this.ev = ev;
+            this.elem = elem;
+            this.vegyjel = vegyjel;
+            this.rendszam = rendszam;
+            this.felfedezo = felfedezo;
+        }
+
+
+        public string Ev { get => ev; set => ev = value; }
+        public string Elem { get => elem; set => elem = value; }
+        public string Vegyjel { get => vegyjel; set => vegyjel = value; }
+        public int Rendszam { get => rendszam; set => rendszam = value; }
+        public string Felfedezo { get => felfedezo; set => felfedezo = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,13 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Doga
 {
     class Program
     {
+        public static List<Elemek> CSVDataList = new List<Elemek>();
         static void Main(string[] args)
         {
+            ReadCSVList();
+
+            //3.feladat:
+            Console.WriteLine($"3. feladat: Elemek száma: {CSVDataList.Select(x => x.Elem).Count()}");
+
+            //4.feladat:
+            //TODO hiba
+            Console.WriteLine($"4. feladat: Felfedezések száma az ókorban: {CSVDataList.Count(x => x.Ev == "Ókor")}");
+
+            //5.feladat:
+            string userData;
+            do
+            {
+                Console.WriteLine("5. feladat: Kérek egy vegyjelet: ");
+                userData = Console.ReadLine();
+
+                if (CharacterCorrector(userData))
+                {
+                    break;
+                }
+            } while (true);
+
+            //6. feladat:
+            List<Elemek> datasByUserdata = CSVDataList.Where(x => x.Vegyjel == userData).ToList();
+            Console.WriteLine(
+                $"6. feladat Keresés \n \t Az elem vegyjele: {datasByUserdata.Select(x => x.Vegyjel)} \n \t Az elem neve: {datasByUserdata.Select(x => x.Elem)} \n \t Rendszáma: {datasByUserdata.Select(x => x.Rendszam)} \n \t Felfedezés éve: {datasByUserdata.Select(x => x.Ev)} \n \t Felfedezős: {datasByUserdata.Select(x => x.Felfedezo)}");
+
+            //7.feladat:
+            Console.WriteLine($"7. feladat: év volt a leghosszabb időszak két elem felfedezéseközött.");
+
+
+            //8.feladat:
+            Console.WriteLine($"8. feladat: {CSVDataList.Where(x => x.Elem.Count() > 3).Select(x => x.Ev).ToString()}");
+
         }
+
+        static void ReadCSVList()
+        {
+             CSVDataList = File.ReadLines("felfedezesek.csv").Skip(1).Select(x => new Elemek(x)).ToList();
+        }
+
+        static bool CharacterCorrector(string data)
+        {
+            char[] characters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            foreach (var item in data.ToLower())
+            {
+                if (!characters.Contains(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
     }
 }
